46eac6426f37af4f7849fbf73309229fc2e449c3
king-ojay <o.attah@alustudent.com>
2025-01-25 09:34:12 +0100

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,11 @@
 -- create the data base if it doesnt exit
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create a new use in the localhost COMMENT
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grantt the user hbnb_dev all privileges on the database hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant all Select Privileges on performance_schema to hbnb_dev
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
