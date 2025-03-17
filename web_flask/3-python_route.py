8525c810673cb68c9471089d7135b4543dd159a6
root <o.attah@alustudent.com>
2025-03-17 18:25:33 +0000

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -19,9 +19,16 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text_var(text):
+def text_route(text):
     """Comment"""
     return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def text_route_python(text="is cool"):
+    """Comment"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
